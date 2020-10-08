69f00e9b9bc04d7c47055c77c3577a69695f4932
Gerald Versluis <gerald@verslu.is>
2020-10-08 15:38:47 +0200

    Implement Connectivity

--- XFEConnectivitySample/MainPage.xaml.cs
+++ XFEConnectivitySample/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace XFEConnectivitySample
@@ -13,6 +14,25 @@ namespace XFEConnectivitySample
         public MainPage()
         {
             InitializeComponent();
+
+            //networkState.Text = Connectivity.NetworkAccess.ToString();
+
+            //if (Connectivity.NetworkAccess == NetworkAccess.None)
+            //{
+            //    networkState.Text = "Please Connect First";
+            //}
+
+            Connectivity.ConnectivityChanged += ConnectivityChangedHandler;
+        }
+
+        private void ConnectivityChangedHandler(object sender, ConnectivityChangedEventArgs e)
+        {
+            networkState.Text = e.NetworkAccess.ToString();
+
+            if (!e.ConnectionProfiles.Contains(ConnectionProfile.Bluetooth))
+            {
+                // TODO Handle Bluetooth
+            }
         }
     }
 }
